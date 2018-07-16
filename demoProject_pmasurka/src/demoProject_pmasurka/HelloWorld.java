2ddf6e23b2a929a3121c11357eba0c4053953662
trainee <trainee>
2018-07-16 16:16:42 +0530

    One more new print statement added

--- demoProject_pmasurka/src/demoProject_pmasurka/HelloWorld.java
+++ demoProject_pmasurka/src/demoProject_pmasurka/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Welcome to CI-CD Training of DevOps");
+		System.out.println("New line added");
 
 	}
 
