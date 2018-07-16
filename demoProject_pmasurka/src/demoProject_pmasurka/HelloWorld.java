4491603be89856e855013c27362e9c049ecaa3c6
trainee <trainee>
2018-07-16 17:04:52 +0530

    Hello World file added again

---
+++ demoProject_pmasurka/src/demoProject_pmasurka/HelloWorld.java
@@ -0,0 +1,13 @@
+package demoProject_pmasurka;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Welcome to CI-CD Training of DevOps");
+		
+
+	}
+
+}
